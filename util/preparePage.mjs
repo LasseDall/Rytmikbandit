5d0f9f21aec5da80a851bd21f3020f79cd1c71c6
LasseDall <lass794rrstud.kea.dk>
2023-10-18 19:23:59 +0200

    ...

--- util/preparePage.mjs
+++ util/preparePage.mjs
@@ -1,33 +1,33 @@
 import { readPage, renderPage } from "./templateEngine.mjs";
 
-const frontpage = readPage("public/pages/frontpage/frontpage.html");
+const frontpage = readPage("/public/pages/frontpage/frontpage.html");
 export const frontpagePage = renderPage(frontpage);
 
-const leg = readPage("public/pages/leg/leg.html");
+const leg = readPage("/public/pages/leg/leg.html");
 export const legPage = renderPage(leg, {
     id: 'id="5"',
     stylesheet:  '<link rel="stylesheet" href="/assets/css/pejlemærke.css"></link>'
 });
 
-const bevægelse = readPage("public/pages/bevægelse/bevægelse.html");
+const bevægelse = readPage("/public/pages/bevægelse/bevægelse.html");
 export const bevægelsePage = renderPage(bevægelse, {
     id: 'id="5"',
     stylesheet:  '<link rel="stylesheet" href="/assets/css/pejlemærke.css"></link>'
 });
 
-const musik = readPage("public/pages/musik/musik.html");
+const musik = readPage("/public/pages/musik/musik.html");
 export const musikPage = renderPage(musik, {
     id: 'id="5"',
     stylesheet:  '<link rel="stylesheet" href="/assets/css/pejlemærke.css"></link>'
 });
 
-const sprog = readPage("public/pages/sprog/sprog.html");
+const sprog = readPage("/public/pages/sprog/sprog.html");
 export const sprogPage = renderPage(sprog, {
     id: 'id="5"',
     stylesheet:  '<link rel="stylesheet" href="/assets/css/pejlemærke.css"></link>'
 });
 
-const fællesskab = readPage("public/pages/fællesskab/fællesskab.html");
+const fællesskab = readPage("/public/pages/fællesskab/fællesskab.html");
 export const fællesskabPage = renderPage(fællesskab, {
     id: 'id="5"',
     stylesheet:  '<link rel="stylesheet" href="/assets/css/pejlemærke.css"></link>'
