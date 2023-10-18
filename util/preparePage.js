183709084efb44e926967aa372c1d9d107a99ebb
LasseDall <lass794rrstud.kea.dk>
2023-10-18 22:27:48 +0200

    godnat

--- util/preparePage.js
+++ util/preparePage.js
@@ -4,40 +4,40 @@ const path = require("path");
 const frontFilePath = path.join(process.cwd(), "/public/pages/frontpage/frontpage.html");
 const frontpage = readPage(frontFilePath);
 exports.frontpagePage = renderPage(frontpage, {
-    stylesheet: `<link rel="stylesheet" href="/public/assets/css/pejlemærke.css"></link>`
+    stylesheet: `<link rel="stylesheet" href="./assets/css/pejlemærke.css"></link>`
 });
 
 const legFilePath = path.join(process.cwd(), "/public/pages/leg/leg.html");
 const leg = readPage(legFilePath);
 exports.legPage = renderPage(leg, {
     id: 'id="5"',
-    stylesheet:  `<link rel="stylesheet" href="/public/assets/css/pejlemærke.css></link>`
+    stylesheet:  `<link rel="stylesheet" href="./assets/css/pejlemærke.css"></link>`
 });
 
 const bevægelseFilePath = path.join(process.cwd(), "/public/pages/bevægelse/bevægelse.html");
 const bevægelse = readPage(bevægelseFilePath);
 exports.bevægelsePage = renderPage(bevægelse, {
     id: 'id="5"',
-    stylesheet:  `<link rel="stylesheet" href="/public/assets/css/pejlemærke.css></link>`
+    stylesheet:  `<link rel="stylesheet" href="./assets/css/pejlemærke.css"></link>`
 });
 
 const musikFilePath = path.join(process.cwd(), "/public/pages/musik/musik.html");
 const musik = readPage(musikFilePath);
 exports.musikPage = renderPage(musik, {
     id: 'id="5"',
-    stylesheet:  `<link rel="stylesheet" href="/public/assets/css/pejlemærke.css></link>`
+    stylesheet:  `<link rel="stylesheet" href="./assets/css/pejlemærke.css"></link>`
 });
 
 const sprogFilePath = path.join(process.cwd(), "/public/pages/sprog/sprog.html");
 const sprog = readPage(sprogFilePath);
 exports.sprogPage = renderPage(sprog, {
     id: 'id="5"',
-    stylesheet:  `<link rel="stylesheet" href="/public/assets/css/pejlemærke.css></link>`
+    stylesheet:  `<link rel="stylesheet" href="./assets/css/pejlemærke.css"></link>`
 });
 
 const fællesskabFilePath = path.join(process.cwd(), "/public/pages/fællesskab/fællesskab.html");
 const fællesskab = readPage(fællesskabFilePath);
 exports.fællesskabPage = renderPage(fællesskab, {
     id: 'id="5"',
-    stylesheet:  `<link rel="stylesheet" href="/public/assets/css/pejlemærke.css></link>`
+    stylesheet:  `<link rel="stylesheet" href="./assets/css/pejlemærke.css"></link>`
 });
