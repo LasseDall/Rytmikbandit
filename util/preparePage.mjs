ec30618498b4b3048b42f088e02a4ae417785537
LasseDall <lass794rrstud.kea.dk>
2023-10-18 20:03:18 +0200

    rootPath.mjs added

--- util/preparePage.mjs
+++ util/preparePage.mjs
@@ -1,34 +1,34 @@
 import { readPage, renderPage } from "./templateEngine.mjs";
 import { rootPath } from "../rootPath.mjs";
 
-const frontpage = readPage(`file://${rootPath}/public/pages/frontpage/frontpage.html`);
+const frontpage = readPage(`${rootPath}/public/pages/frontpage/frontpage.html`);
 export const frontpagePage = renderPage(frontpage);
 
-const leg = readPage(`file://${rootPath}/public/pages/leg/leg.html`);
+const leg = readPage(`${rootPath}/public/pages/leg/leg.html`);
 export const legPage = renderPage(leg, {
     id: 'id="5"',
     stylesheet:  '<link rel="stylesheet" href="/assets/css/pejlemærke.css"></link>'
 });
 
-const bevægelse = readPage(`file://${rootPath}/public/pages/bevægelse/bevægelse.html`);
+const bevægelse = readPage(`${rootPath}/public/pages/bevægelse/bevægelse.html`);
 export const bevægelsePage = renderPage(bevægelse, {
     id: 'id="5"',
     stylesheet:  '<link rel="stylesheet" href="/assets/css/pejlemærke.css"></link>'
 });
 
-const musik = readPage(`file://${rootPath}/public/pages/musik/musik.html`);
+const musik = readPage(`${rootPath}/public/pages/musik/musik.html`);
 export const musikPage = renderPage(musik, {
     id: 'id="5"',
     stylesheet:  '<link rel="stylesheet" href="/assets/css/pejlemærke.css"></link>'
 });
 
-const sprog = readPage(`file://${rootPath}/public/pages/sprog/sprog.html`);
+const sprog = readPage(`${rootPath}/public/pages/sprog/sprog.html`);
 export const sprogPage = renderPage(sprog, {
     id: 'id="5"',
     stylesheet:  '<link rel="stylesheet" href="/assets/css/pejlemærke.css"></link>'
 });
 
-const fællesskab = readPage(`file://${rootPath}/public/pages/fællesskab/fællesskab.html`);
+const fællesskab = readPage(`${rootPath}/public/pages/fællesskab/fællesskab.html`);
 export const fællesskabPage = renderPage(fællesskab, {
     id: 'id="5"',
     stylesheet:  '<link rel="stylesheet" href="/assets/css/pejlemærke.css"></link>'
